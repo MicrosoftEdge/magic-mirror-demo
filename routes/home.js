f2dc9626d2a0306dde4b7d25dca6acf214bfaa58
U-REDMOND\apavia <apavia@DESKTOP-R0SMQN8.redmond.corp.microsoft.com>
2016-02-23 12:46:06 -0800

    adding func to load page specific js file

--- routes/home.js
+++ routes/home.js
@@ -1,7 +1,9 @@
 module.exports = function(app, settings){
-    var url = require('url'),
-        express = require('express'),
-        rootRouter = express.Router()
+    var url = require('url')
+        , express = require('express')
+        , rootRouter = express.Router()
+        , fs = require('fs')
+        , path = require('path')
          
     rootRouter.use(function(req, res, next) {
         next()
@@ -9,6 +11,12 @@ module.exports = function(app, settings){
     
     rootRouter.get('/', function(req, res, next) {
         res.send('Magic miror home using router... go to /create route to personalize and /mirror for mirror UI')
+    })
+    
+    rootRouter.get('/style.css', function(req, res, next) {
+        res.writeHead(200, {'Content-Type': 'text/css'});
+        res.write(fs.readFileSync(path.resolve(__dirname + '/../views/style/style.css'), 'utf8'))
+        res.end()
     })
     
     app.use('/', rootRouter)
