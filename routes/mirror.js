d2164ba5690dc4aada4c0ca5a8f3934f4d87e341
John-David Dalton <john.david.dalton@gmail.com>
2016-02-25 10:49:24 -0800

    Add jquery, moment, and weather libs.

--- routes/mirror.js
+++ routes/mirror.js
@@ -1,9 +1,9 @@
 module.exports = function(app) {
   var url = require('url')
-    , express = require('express')
-    , mirrorRouter = express.Router()
-    , path = require('path')
-    , fs = require('fs');
+      , express = require('express')
+      , mirrorRouter = express.Router()
+      , path = require('path')
+      , fs = require('fs');
 
   mirrorRouter.use(function(req, res, next) {
     next();
@@ -11,13 +11,19 @@ module.exports = function(app) {
 
   mirrorRouter.get('/', function(req, res, next) {
     res.render('./../views/partial/mirror', {});
-  })
+  });
+
+  mirrorRouter.get('/weather.js', function(req, res, next) {
+    res.writeHead(200, {'Content-Type': 'text/js'});
+    res.write(fs.readFileSync(path.resolve(__dirname + '/../views/js/weather.js'), 'utf8'));
+    res.end();
+  });
 
   mirrorRouter.get('/mirror.js', function(req, res, next) {
     res.writeHead(200, {'Content-Type': 'text/js'});
     res.write(fs.readFileSync(path.resolve(__dirname + '/../views/js/mirror.js'), 'utf8'));
     res.end();
-  })
+  });
 
   app.use('/mirror', mirrorRouter);
 };
