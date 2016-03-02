7c62c726013f3ba7b032df94183d29b041f8db18
Kevin Hill <kevinhill@live.com>
2016-03-02 13:59:17 -0800

    Adding back routes

---
+++ routes/createProfileRoute.js
@@ -0,0 +1,57 @@
+module.exports = function(app) {
+  var express = require('express')
+    , createUserRouter = express.Router()
+    , formidable = require("formidable")
+    , mongoose = require('mongoose')
+    , util = require('util')
+    , url = require('url')
+    , fs = require('fs')
+    , path = require('path')
+    , bandname = require('bandname');
+
+  var Person = mongoose.model('Person');
+
+  createUserRouter.use(function(req, res, next) {
+    next();
+  });
+
+  createUserRouter.get('/', function(req, res, next) {
+    res.render('./../views/partial/createProfile', {
+        helpers:{ 
+            getBandName: function() { return bandname();},
+            getZipcode: function() {return '98052';},
+            getEmail: function() {return 'email@outlook.com';}
+        }
+    });
+  });
+
+  createUserRouter.get('/createProfile.js', function(req, res, next) {
+    res.writeHead(200, { 'Content-Type': 'text/js' });
+    res.write(fs.readFileSync(path.resolve(__dirname + '/../views/js/createProfile.js'), 'utf8'));
+    res.end();
+  });
+
+  createUserRouter.post('/', function(req, res, next) {
+    processAllFieldsOfTheForm(req, res)
+  });
+
+  function processAllFieldsOfTheForm(req, res) {
+    var form = new formidable.IncomingForm();    
+    form.parse(req, function (err, fields, files) {
+      //Store the data from the fields in your data store.
+      //The data store could be a file or database or any other store based
+      //on your application.
+      res.writeHead(200, {
+        'content-type': 'text/plain'
+      })
+      var person = new Person(fields)
+      person.save(function (err, fluffy) {        
+        if (err) return console.error(err)
+      })
+      res.write(JSON.stringify(person._id))
+      res.end()
+    });
+  }
+
+  app.use('/create', createUserRouter);
+};
