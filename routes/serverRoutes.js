8cd7073aeb1a2d1feb63ffe5d51ae50133817b95
Grisha Lyukshin <glyuk@microsoft.com>
2016-03-07 18:58:16 -0800

    my files

--- routes/serverRoutes.js
+++ routes/serverRoutes.js
@@ -5,4 +5,5 @@ module.exports = function(app) {
     require('./createProfileRoute')(app);
     require('./mirrorRoute')(app);
     require('./captureFaceRoute')(app);
+    //require('./stockRoute')(app); TODO: this is probable not needed. Using mirror router should suffice
 };
