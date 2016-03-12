a67b5f091cf277d92a7c7c3ae60c04a1d7f0aeb1
Kiril Seksenov <seksenovk@gmail.com>
2016-03-11 16:01:33 -0800

    fixing authenticate

--- views/js/authenticate.js
+++ views/js/authenticate.js
@@ -37,6 +37,20 @@ var prevX, prevY, prevWidth, prevHeight;
 var mediaStreamType = Capture.MediaStreamType.videoRecord;
 var timeoutSet = false;
 
+function Uint8ToBase64(u8Arr) {
+            var CHUNK_SIZE = 0x8000;
+            var index = 0;
+            var length = u8Arr.length;
+            var result = "";
+            var slice;
+            while (index < length) {
+                slice = u8Arr.subarray(index, Math.min(index + CHUNK_SIZE, length));
+                result += String.fromCharCode.apply(null, slice);
+                index += CHUNK_SIZE;
+            }
+            return btoa(result);
+        }
+
 function isStable(face) {
   if (stabilizationCounter == cycles) {
     prevX = prevY = prevWidth = prevHeight = null;
