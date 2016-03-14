791097ab6190ccf013b1c08e0a764dee62e7f76f
jayrenn <joshrenn@microsoft.com>
2016-03-14 12:30:50 -0700

    use jsonp for stock ajax request

--- views/js/stock.js
+++ views/js/stock.js
@@ -11,61 +11,66 @@
         var watchList, refresh;
 
         function getQuotes() {
-            $.get(url + encodeStocks, function (data) {
-                var stockData = JSON.parse(data.substr(3));
-                stockData.forEach(function (stock) {
-                    var symbol = stock.t;
-                    var lastPrice = stock.l;
-                    var changePercentage = stock.cp;
-                    var changeSinceClose = stock.c;
-                    var ticker;
-                    var symbolLabel;
-                    var tickerPrice;
-                    var tickerChange;
+            $.ajax({
+                type: "GET",
+                url: url + encodeStocks,
+                dataType: "jsonp",
+                success: function (data) {
+                    data.forEach(function (stock) {
+                        var symbol = stock.t;
+                        var lastPrice = stock.l;
+                        var changePercentage = stock.cp;
+                        var changeSinceClose = stock.c;
+                        var ticker;
+                        var symbolLabel;
+                        var tickerPrice;
+                        var tickerChange;
 
-                    // Set up initial ticker item
-                    if (!initialized) {
-                        ticker = document.createElement("li");
-                        ticker.classList.add("ticker");
-                        ticker.id = symbol;
+                        // Set up initial ticker item
+                        if (!initialized) {
+                            ticker = document.createElement("li");
+                            ticker.classList.add("ticker");
+                            ticker.id = symbol;
 
-                        symbolLabel = document.createElement("div");
-                        symbolLabel.classList.add("symbol");
-                        ticker.appendChild(symbolLabel);
-                        symbolLabel.innerText = `${symbol}`;
+                            symbolLabel = document.createElement("div");
+                            symbolLabel.classList.add("symbol");
+                            ticker.appendChild(symbolLabel);
+                            symbolLabel.innerText = `${symbol}`;
 
-                        tickerPrice = document.createElement("div");
-                        tickerPrice.classList.add("price");
-                        ticker.appendChild(tickerPrice);
+                            tickerPrice = document.createElement("div");
+                            tickerPrice.classList.add("price");
+                            ticker.appendChild(tickerPrice);
 
-                        tickerChange = document.createElement("div");
-                        tickerChange.classList.add("price-change");
-                        ticker.appendChild(tickerChange);
+                            tickerChange = document.createElement("div");
+                            tickerChange.classList.add("price-change");
+                            ticker.appendChild(tickerChange);
 
-                    }
-                    else {
-                        ticker = document.getElementById(symbol);
-                    }
+                        }
+                        else {
+                            ticker = document.getElementById(symbol);
+                        }
 
-                    // Update price and change values
-                    tickerPrice.innerText = `${lastPrice}`;
-                    tickerChange.innerText = `${changePercentage}%`;
+                        // Update price and change values
+                        tickerPrice.innerText = `${lastPrice}`;
+                        tickerChange.innerText = `${changePercentage}%`;
 
-                    // Show positive or negative change icon
-                    if (`${changePercentage}` > 0 && !tickerChange.classList.contains("pos-change")) {
-                      tickerChange.classList.add("pos-change");
-                      tickerChange.classList.remove("neg-change");
-                    } else if (`${changePercentage}` < 0 && !tickerChange.classList.contains("neg-change")) {
-                      tickerChange.classList.add("neg-change");
-                      tickerChange.classList.remove("pos-change");
-                    }
+                        // Show positive or negative change icon
+                        if (`${changePercentage}` > 0 && !tickerChange.classList.contains("pos-change")) {
+                          tickerChange.classList.add("pos-change");
+                          tickerChange.classList.remove("neg-change");
+                        } else if (`${changePercentage}` < 0 && !tickerChange.classList.contains("neg-change")) {
+                          tickerChange.classList.add("neg-change");
+                          tickerChange.classList.remove("pos-change");
+                        }
 
-                    // Add initial ticker item
-                    if (!initialized) {
-                        watchList.appendChild(ticker);
-                    }
-                });
-            }).done(function () {
+                        // Add initial ticker item
+                        if (!initialized) {
+                            watchList.appendChild(ticker);
+                        }
+                    });
+                }
+            })
+            .done(function () {
                 if (!initialized) {
                     initialized = true;
                 }
