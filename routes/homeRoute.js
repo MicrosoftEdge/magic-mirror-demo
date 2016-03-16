ed9f94fdf39d6360c3135c9e0500c724160e837b
Melanie Richards <merichar@microsoft.com>
2016-03-15 18:25:40 -0700

    Move images and styles to public directory
    More common and expected organizational style.

--- routes/homeRoute.js
+++ routes/homeRoute.js
@@ -13,18 +13,6 @@ module.exports = function(app, settings) {
     res.send('Magic miror home using router... go to <a href="/create">/create</a> route to personalize and <a href="/mirror">/mirror</a> for mirror UI');
   });
 
-  rootRouter.get('/style.css', function(req, res, next) {
-    res.writeHead(200, {'Content-Type': 'text/css'});
-    res.write(fs.readFileSync(path.resolve(__dirname + '/../views/style/style.css'), 'utf8'));
-    res.end();
-  });
-
-  rootRouter.get('/jquery-ui-reduced.css', function(req, res, next) {
-    res.writeHead(200, {'Content-Type': 'text/css'});
-    res.write(fs.readFileSync(path.resolve(__dirname + '/../views/style/jquery-ui-reduced.css'), 'utf8'));
-    res.end();
-  });
-
   rootRouter.get('/setupUtilities.js', function(req, res, next) {
     res.writeHead(200, { 'Content-Type': 'text/js' });
     res.write(fs.readFileSync(path.resolve(__dirname + '/../views/js/setupUtilities.js'), 'utf8'));
